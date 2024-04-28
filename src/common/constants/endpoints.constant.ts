c82a2dd2baf894ea2fa6cb05635c30f9f1895a88
RamirezPineda <ramirezpineda@gmail.com>
2024-04-28 09:38:28 -0400

    feat(auth): add auth endpoints

--- src/common/constants/endpoints.constant.ts
+++ src/common/constants/endpoints.constant.ts
@@ -1,5 +1,7 @@
-export const PATH_PREFIX = '/api/v1.0';
+export const PATH_PREFIX = '/api/v1';
 
 export enum ENDPOINTS {
   HEALTH = '/health',
+  AUTH_REGISTER = '/auth/register',
+  AUTH_LOGIN = '/auth/login',
 }
