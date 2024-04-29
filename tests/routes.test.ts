fbf1b7e1c06970e22cce95296e9f6c1434e6edcf
RamirezPineda <ramirezpineda@gmail.com>
2024-04-29 18:42:23 -0400

    feat: add server to test

--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -1,20 +1,17 @@
 import request from 'supertest';
 
-import { Routes } from '@/routes';
-import { Server } from '@/server';
-import { EnvConfig } from '@/config';
 import { ENDPOINTS, PATH_PREFIX } from '@/common/constants';
-
-const testServer = new Server({
-  port: EnvConfig.PORT,
-  routes: Routes.routes,
-});
+import { testServer } from '@tests/test-server';
 
 const healthUrl = PATH_PREFIX + ENDPOINTS.HEALTH;
 
 describe('Testing routes.ts', async () => {
   beforeAll(async () => {
     await testServer.start();
+  });
+
+  afterAll(() => {
+    testServer.close();
   });
 
   test('should return status 200 | GET ' + healthUrl, async () => {
