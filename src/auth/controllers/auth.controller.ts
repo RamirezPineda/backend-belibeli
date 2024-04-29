7ff7c243030a8ebd2e473522b0165559e79babcc
RamirezPineda <ramirezpineda@gmail.com>
2024-04-28 21:10:24 -0400

    feat(auth): add register and login function

---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,40 @@
+import type { Request, Response } from 'express';
+
+import type { ResponseApi } from '@/common/interfaces';
+import { handlerErrors } from '@/common/utils';
+
+import { AuthService } from '@/auth/services/auth.service';
+import type { LoginDto } from '@/auth/dto/login.dto';
+import type { UserCreateDto } from '@/users/dto';
+
+export class AuthController {
+  constructor(private readonly authService: AuthService) {
+    this.authService = authService;
+  }
+
+  async register(req: Request, res: Response) {
+    try {
+      const createUserDto: UserCreateDto = req.body;
+      const newUser = await this.authService.register(createUserDto);
+      const responseApi: ResponseApi = { statusCode: 201, data: newUser };
+
+      res.status(201).json(responseApi);
+    } catch (error) {
+      handlerErrors(res, error);
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    try {
+      const loginDto: LoginDto = req.body.data;
+      const { user, token } = await this.authService.login(loginDto);
+
+      res.header('Authorization', `Bearer ${token}`);
+      const responseApi: ResponseApi = { statusCode: 200, data: user };
+
+      res.status(200).json(responseApi);
+    } catch (error) {
+      handlerErrors(res, error);
+    }
+  }
+}
