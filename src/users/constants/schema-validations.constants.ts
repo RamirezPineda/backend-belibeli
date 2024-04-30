dd62cb656a270a7dd296b79b802a29f46b25100c
RamirezPineda <ramirezpineda@gmail.com>
2024-04-29 21:36:17 -0400

    feat: add validation constants

---
+++ src/users/constants/schema-validations.constants.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+
+export const validateEmail = z
+  .string({ message: 'Email is required' })
+  .email({ message: 'The email is invalid' });
+
+export const validateName = z
+  .string({ message: 'Name is required' })
+  .min(3, {
+    message: 'The name must be greater than or equal to 3 characters.',
+  })
+  .max(50, {
+    message: 'The name must be less than or equal to 50 characters.',
+  });
+
+export const validatePassword = z
+  .string({ message: 'Password is required' })
+  .min(8, {
+    message: 'The password must be greater than or equal to 8 characters.',
+  })
+  .max(50, {
+    message: 'The password must be less than or equal to 50 characters.',
+  });
+
+export const validateIsActive = z.boolean({
+  message: 'IsActive must be of type boolean',
+});
