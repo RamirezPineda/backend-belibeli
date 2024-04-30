e97d9c5b8f65926f80872e19de20fd69823f4763
RamirezPineda <ramirezpineda@gmail.com>
2024-04-30 18:10:10 -0400

    feat: add convert to query function

---
+++ src/common/utils/convert-to-query.utils.ts
@@ -0,0 +1,20 @@
+import { OrderEnum, type Query, type QueryOptions } from '@common/interfaces';
+
+export const convertToQuery = (queryOptions: QueryOptions): Query => {
+  const { offset, limit, attr, value, order } = queryOptions;
+
+  const pagination = { skip: offset, take: limit };
+  const orderBy = order ? { createdAt: order } : { createdAt: OrderEnum.desc };
+
+  let where = {};
+  if (attr && value) {
+    if (typeof value == 'boolean') {
+      where = { [attr]: value };
+    } else {
+      where = { [attr]: { contains: value, mode: 'insensitive' } };
+    }
+  }
+
+  const query = { ...pagination, orderBy, where };
+  return query;
+};
--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './response-error.utils';
 export * from './handler-errors.utils';
 export * from './exclude-attributes.utils';
+export * from './convert-to-query.utils';
