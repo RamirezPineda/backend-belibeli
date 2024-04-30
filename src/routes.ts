94aae9a47f8dab1c990ae0cccabe933c25daf479
RamirezPineda <ramirezpineda@gmail.com>
2024-04-30 18:16:20 -0400

    feat(users): add enpoint users

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { ENDPOINTS } from '@common/constants';
 import { AuthRoutes } from '@/auth/routes/auth.routes';
+import { UserRoutes } from '@/users/routes/user.routes';
 
 export class Routes {
   public static get routes(): Router {
@@ -11,6 +12,7 @@ export class Routes {
     router.get(ENDPOINTS.HEALTH, (_, res) => res.status(200).json());
 
     router.use(AuthRoutes.routes);
+    router.use(UserRoutes.routes);
 
     return router;
   }
