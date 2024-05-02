f66782d42435354263b737c725c963bb46c6add3
RamirezPineda <ramirezpineda@gmail.com>
2024-05-02 10:07:52 -0400

    feat(packages): add crud packages

---
+++ src/packages/services/package.service.ts
@@ -0,0 +1,43 @@
+import {
+  ResponseError,
+  convertToQuery,
+  removeValuesUndefined,
+} from '@/common/utils';
+import type { QueryOptions } from '@/common/interfaces';
+
+import { PackageRepository } from '@/packages/repositories/package.repository';
+import { PackageCreateDto, PackageUpdateDto } from '@/packages/dto';
+
+export class PackageService {
+  constructor(private readonly packageReposiory: PackageRepository) {}
+
+  async findAll(queryOptions: QueryOptions) {
+    const query = convertToQuery(queryOptions);
+    return this.packageReposiory.findAll(query);
+  }
+
+  async create(packageCreateDto: PackageCreateDto) {
+    return this.packageReposiory.createPackage(packageCreateDto);
+  }
+
+  async findById(id: string) {
+    const packageFound = await this.packageReposiory.findById(id);
+
+    if (!packageFound) {
+      throw new ResponseError({ messages: ['Package not found'] });
+    }
+
+    return packageFound;
+  }
+
+  async update(id: string, packageUpdateDto: PackageUpdateDto) {
+    const packageFound = await this.findById(id);
+    const data = removeValuesUndefined(packageUpdateDto);
+    return this.packageReposiory.update(packageFound.id, data);
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+    return this.packageReposiory.delete(id);
+  }
+}
