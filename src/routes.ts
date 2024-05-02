403cfd572d93abb9f0c911d735bd7ba9ba6ef120
RamirezPineda <ramirezpineda@gmail.com>
2024-05-02 10:08:50 -0400

    feat(packages): add packages endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { ENDPOINTS } from '@common/constants';
 import { AuthRoutes } from '@/auth/routes/auth.routes';
 import { UserRoutes } from '@/users/routes/user.routes';
 import { CategoryRoutes } from '@/categories/routes/category.routes';
+import { PackageRoutes } from '@/packages/routes/package.routes';
 
 export class Routes {
   public static get routes(): Router {
@@ -15,6 +16,7 @@ export class Routes {
     router.use(AuthRoutes.routes);
     router.use(UserRoutes.routes);
     router.use(CategoryRoutes.routes);
+    router.use(PackageRoutes.routes);
 
     return router;
   }
