8aa3e685fa4bc32362e7912bc8b1123674949667
RamirezPineda <ramirezpineda@gmail.com>
2024-05-02 07:36:11 -0400

    feat: add cloudinary environment

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,6 +2,7 @@ process.loadEnvFile();
 
 export interface EnvConfigProps {
   NODE_ENV: string;
+  PORT: number;
   DB_USER: string;
   DB_PASSWORD: string;
   DB_NAME: string;
@@ -10,11 +11,14 @@ export interface EnvConfigProps {
   JWT_SECRET: string;
   JWT_EXPIRATION: string;
   SALT_ROUNDS: number;
-  PORT: number;
+  CLOUDINARY_CLOUD_NAME: string;
+  CLOUDINARY_API_KEY: string;
+  CLOUDINARY_API_SECRET: string;
 }
 
 export const EnvConfig: EnvConfigProps = {
   NODE_ENV: process.env.NODE_ENV ?? 'development',
+  PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
   DB_USER: process.env.DB_USER ?? '',
   DB_PASSWORD: process.env.DB_PASSWORD ?? '',
   DB_NAME: process.env.DB_NAME ?? '',
@@ -23,5 +27,7 @@ export const EnvConfig: EnvConfigProps = {
   JWT_SECRET: process.env.JWT_SECRET ?? '',
   JWT_EXPIRATION: process.env.JWT_EXPIRATION ?? '',
   SALT_ROUNDS: process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 8,
-  PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME ?? '',
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY ?? '',
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET ?? '',
 };
