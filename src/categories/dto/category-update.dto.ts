c1b5a0a5045ecef8985897dcb89d3e8b517af009
RamirezPineda <ramirezpineda@gmail.com>
2024-05-02 08:02:12 -0400

    feat(categories): add update category dto

--- src/categories/dto/category-update.dto.ts
+++ src/categories/dto/category-update.dto.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const categoryCreateDtoSchema = z.object({
+export const categoryUpdateDtoSchema = z.object({
   name: z
     .string({ message: 'Name is required' })
     .min(3, {
@@ -8,7 +8,8 @@ export const categoryCreateDtoSchema = z.object({
     })
     .max(50, {
       message: 'The name must be less than or equal to 50 characters.',
-    }),
+    })
+    .optional(),
   description: z
     .string({ message: 'Description must be of type string' })
     .min(3, {
@@ -18,6 +19,10 @@ export const categoryCreateDtoSchema = z.object({
       message: 'The description must be less than or equal to 50 characters.',
     })
     .optional(),
+  imageUrl: z
+    .string({ message: 'ImageUrl must be of type string' })
+    .startsWith('http', { message: 'ImageUrl must start with http' })
+    .optional(),
 });
 
-export type CategoryCreateDto = z.infer<typeof categoryCreateDtoSchema>;
+export type CategoryUpdateDto = z.infer<typeof categoryUpdateDtoSchema>;
--- src/categories/dto/index.ts
+++ src/categories/dto/index.ts
@@ -1,1 +1,2 @@
 export * from './category-create.dto';
+export * from './category-update.dto';
