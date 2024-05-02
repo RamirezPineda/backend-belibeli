bca69ba3a21e9a2d7ebfe099ef6325701c92b8a7
RamirezPineda <ramirezpineda@gmail.com>
2024-05-02 17:18:39 -0400

    feat(products): add find all, create and find by id functions

---
+++ src/products/repositories/product.repository.ts
@@ -0,0 +1,31 @@
+import { prisma } from '@/common/database/conection.database';
+import { Query } from '@/common/interfaces';
+
+import type { ProductCreateDto, ProductImageCreateDto } from '@/products/dto';
+import type { Product } from '@/products/models';
+
+export class ProductRepository {
+  async findAll(query: Query): Promise<Product[]> {
+    return prisma.product.findMany({
+      ...query,
+      include: { productImage: true },
+    });
+  }
+
+  async create(
+    productCreateDto: ProductCreateDto,
+    productImageCreateDto: ProductImageCreateDto[],
+  ): Promise<Product> {
+    return prisma.product.create({
+      data: {
+        ...productCreateDto,
+        productImage: { create: productImageCreateDto },
+      },
+      include: { productImage: true },
+    });
+  }
+
+  async findById(id: string): Promise<Product | null> {
+    return prisma.product.findUnique({ where: { id } });
+  }
+}
