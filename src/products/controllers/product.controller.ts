5675422f39740e07216a33bcbf02cb6cbf3d1413
RamirezPineda <ramirezpineda@gmail.com>
2024-05-03 10:07:30 -0400

    feat(products): add product update function

--- src/products/controllers/product.controller.ts
+++ src/products/controllers/product.controller.ts
@@ -4,7 +4,7 @@ import type { QueryOptions, ResponseApi } from '@/common/interfaces';
 import { handlerErrors } from '@/common/utils';
 
 import { ProductService } from '@/products/services/product.service';
-import type { ProductCreateDto } from '@/products/dto';
+import type { ProductCreateDto, ProductUpdateDto } from '@/products/dto';
 
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
@@ -43,6 +43,29 @@ export class ProductController {
       const { id } = req.params;
       const productFound = await this.productService.findById(id);
       const responseApi: ResponseApi = { statusCode: 200, data: productFound };
+
+      res.status(200).json(responseApi);
+    } catch (error) {
+      handlerErrors(res, error);
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const productUpdateDto: ProductUpdateDto = req.body.data;
+      const images: Express.Multer.File[] = req.files as Express.Multer.File[];
+
+      const productUpdated = await this.productService.update(
+        id,
+        productUpdateDto,
+        images,
+      );
+
+      const responseApi: ResponseApi = {
+        statusCode: 200,
+        data: productUpdated,
+      };
 
       res.status(200).json(responseApi);
     } catch (error) {
--- src/products/services/product.service.ts
+++ src/products/services/product.service.ts
@@ -1,15 +1,29 @@
-import { type UploadApiResponse } from 'cloudinary';
+import type { QueryOptions } from '@common/interfaces';
+import {
+  ResponseError,
+  convertToQuery,
+  deleteFile,
+  excludeAttributes,
+  removeValuesUndefined,
+  UploadFile,
+} from '@common/utils';
 
-import { QueryOptions } from '@common/interfaces';
-import { ResponseError, convertToQuery, deleteFile } from '@common/utils';
-import { UploadFile } from '@common/utils/upload-file.utils';
-
-import { ProductRepository } from '@/products/repositories/product.repository';
+import {
+  ProductRepository,
+  ProductImageRepository,
+} from '@/products/repositories';
 import type { Product } from '@/products/models';
-import type { ProductCreateDto, ProductImageCreateDto } from '@/products/dto';
+import type {
+  ProductCreateDto,
+  ProductImageCreateDto,
+  ProductUpdateDto,
+} from '@/products/dto';
 
 export class ProductService {
-  constructor(private readonly productRepository: ProductRepository) {}
+  constructor(
+    private readonly productRepository: ProductRepository,
+    private readonly productImageRepository: ProductImageRepository,
+  ) {}
 
   async findAll(queryOptions: QueryOptions): Promise<Product[]> {
     const query = convertToQuery(queryOptions);
@@ -20,25 +34,14 @@ export class ProductService {
     productCreateDto: ProductCreateDto,
     images: Express.Multer.File[],
   ) {
-    const productImageCreateDto: ProductImageCreateDto[] = [];
-    const promiseImages: Promise<UploadApiResponse>[] = [];
-    const promiseDeleteImages: Promise<void>[] = [];
+    const promiseImages = images.map((image) => UploadFile.upload(image.path));
+    const uploadedImages = await Promise.all(promiseImages);
 
-    for (const image of images) {
-      promiseImages.push(UploadFile.upload(image.path));
-    }
+    const productImageCreateDto: ProductImageCreateDto[] = uploadedImages.map(
+      (image) => ({ url: image.secure_url }),
+    );
 
-    const uploadedImages: UploadApiResponse[] =
-      await Promise.all(promiseImages);
-
-    for (const image of uploadedImages) {
-      productImageCreateDto.push({ url: image.secure_url });
-    }
-
-    for (const image of images) {
-      promiseDeleteImages.push(deleteFile(image.path));
-    }
-
+    const promiseDeleteImages = images.map((image) => deleteFile(image.path));
     await Promise.all(promiseDeleteImages);
 
     return this.productRepository.create(
@@ -58,5 +61,53 @@ export class ProductService {
     }
 
     return productFound;
+  }
+
+  async update(
+    id: string,
+    productUpdateDto: ProductUpdateDto,
+    images: Express.Multer.File[],
+  ) {
+    const productFound = await this.findById(id);
+
+    const productImageCreateDto: ProductImageCreateDto[] = [];
+
+    if (images.length > 0) {
+      const promiseImages = images.map((image) =>
+        UploadFile.upload(image.path),
+      );
+      const uploadedImages = await Promise.all(promiseImages);
+
+      for (const image of uploadedImages) {
+        productImageCreateDto.push({ url: image.secure_url });
+      }
+
+      const promiseDeleteImages = images.map((image) => deleteFile(image.path));
+      await Promise.all(promiseDeleteImages);
+    }
+
+    if (
+      productUpdateDto.productImages.length !==
+      productFound.productImage!.length
+    ) {
+      const productImagesToDelete = productFound.productImage!.filter(
+        (productImage) =>
+          !productUpdateDto.productImages.includes(productImage.id),
+      );
+      const imagesToDelete = productImagesToDelete.map((productImage) => ({
+        id: productImage.id,
+      }));
+
+      await this.productImageRepository.deleteMany(imagesToDelete);
+    }
+
+    let data = removeValuesUndefined(productUpdateDto);
+    data = excludeAttributes(data, ['productImages']);
+
+    return this.productRepository.update(
+      productFound.id,
+      data,
+      productImageCreateDto,
+    );
   }
 }
