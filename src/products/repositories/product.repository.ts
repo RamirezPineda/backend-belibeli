b618d0707bc5eef147fbf9a0cf14bf27d762087a
RamirezPineda <ramirezpineda@gmail.com>
2024-05-03 10:03:09 -0400

    feat(products): add update function

--- src/products/repositories/product.repository.ts
+++ src/products/repositories/product.repository.ts
@@ -1,7 +1,11 @@
 import { prisma } from '@/common/database/conection.database';
 import { Query } from '@/common/interfaces';
 
-import type { ProductCreateDto, ProductImageCreateDto } from '@/products/dto';
+import type {
+  ProductCreateDto,
+  ProductImageCreateDto,
+  ProductUpdateDto,
+} from '@/products/dto';
 import type { Product } from '@/products/models';
 
 export class ProductRepository {
@@ -26,6 +30,24 @@ export class ProductRepository {
   }
 
   async findById(id: string): Promise<Product | null> {
-    return prisma.product.findUnique({ where: { id } });
+    return prisma.product.findUnique({
+      where: { id },
+      include: { productImage: true },
+    });
+  }
+
+  async update(
+    id: string,
+    productUpdateDto: Omit<ProductUpdateDto, 'productImages'>,
+    productImageCreateDto: ProductImageCreateDto[],
+  ): Promise<Product> {
+    return prisma.product.update({
+      where: { id },
+      data: {
+        ...productUpdateDto,
+        productImage: { create: productImageCreateDto },
+      },
+      include: { productImage: true },
+    });
   }
 }
