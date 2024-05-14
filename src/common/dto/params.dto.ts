0775a15e82e485397333bbab2eaced51a2651a9f
RamirezPineda <ramirezpineda@gmail.com>
2024-05-13 22:29:41 -0400

    feat: add orders endpoints

--- src/common/dto/params.dto.ts
+++ src/common/dto/params.dto.ts
@@ -1,9 +1,22 @@
 import { z } from 'zod';
 
+const idValidate = (fieldName: string) =>
+  z
+    .string({ message: `${fieldName} parameter is required` })
+    .uuid({ message: `${fieldName} must be of type uuid` });
+
 export const paramsDtoSchema = z.object({
-  id: z
-    .string({ message: 'Id parameter is required' })
-    .uuid({ message: 'Id must be of type uuid' }),
+  id: idValidate('Id'),
+});
+
+export const userIdParamsDtoSchema = z.object({
+  userId: idValidate('User Id'),
+});
+
+export const idAndUserIdParamsDtoSchema = paramsDtoSchema.extend({
+  userId: idValidate('User Id'),
 });
 
 export type paramsDto = z.infer<typeof paramsDtoSchema>;
+export type userIdParamsDto = z.infer<typeof userIdParamsDtoSchema>;
+export type idAndUserIdParamsDto = z.infer<typeof idAndUserIdParamsDtoSchema>;
