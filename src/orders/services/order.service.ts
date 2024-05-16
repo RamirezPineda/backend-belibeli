77b46bb7b11487d2eacbe2d0ac25fed5803c8f1b
RamirezPineda <ramirezpineda@gmail.com>
2024-05-16 11:35:19 -0400

    feat(payments): add stripe payment gateway

--- src/orders/services/order.service.ts
+++ src/orders/services/order.service.ts
@@ -3,10 +3,15 @@ import { ResponseError, convertToQuery } from '@/common/utils';
 import type { User } from '@/users/models/user.model';
 
 import { OrderRepository } from '@/orders/repositories/order.repository';
-import type { OrderCreateDto } from '@/orders/dto';
+import { ProductRepository } from '@/products/repositories/product.repository';
+import type { OrderCreateDto, ProductOrderCreateDto } from '@/orders/dto';
+import { StripePayment, calculateProductPrice } from '@/orders/utils';
 
 export class OrderService {
-  constructor(private readonly orderRepository: OrderRepository) {}
+  constructor(
+    private readonly orderRepository: OrderRepository,
+    private readonly productRepository: ProductRepository,
+  ) {}
 
   async findAll(queryOptions: QueryOptions) {
     const query = convertToQuery(queryOptions);
@@ -19,8 +24,31 @@ export class OrderService {
   }
 
   async create(orderCreateDto: OrderCreateDto, user: Omit<User, 'password'>) {
-    // todo: implementar la pasarela de pagos STRIPE
-    return this.orderRepository.create(orderCreateDto, user.id);
+    const { confirmationTokenId, ...restOrderCreateDto } = orderCreateDto;
+
+    const amount = await this.calculateAmount(restOrderCreateDto.productOrder);
+
+    let paymentIntentId = '';
+
+    try {
+      const { id } = await StripePayment.createPaymentIntents({
+        amount,
+        confirmationTokenId,
+      });
+      paymentIntentId = id;
+
+      return this.orderRepository.create(restOrderCreateDto, user.id);
+    } catch (error) {
+      if (paymentIntentId !== '') {
+        await StripePayment.refund({ paymentIntentId, amount });
+      }
+
+      throw new ResponseError({
+        messages: [
+          'Lo siento, no se pudo procesar la orden de compra en este momento debido a problemas técnicos. Por favor, inténtelo de nuevo más tarde.',
+        ],
+      });
+    }
   }
 
   async findById(id: string) {
@@ -44,5 +72,31 @@ export class OrderService {
     }
 
     return orderFound;
+  }
+
+  private async calculateAmount(createProductOrders: ProductOrderCreateDto) {
+    let amount = 0.0;
+
+    const calculateTotalPromise = createProductOrders.map(
+      async (productOrder) => {
+        const productFound = await this.productRepository.findById(
+          productOrder.productId,
+        );
+
+        if (!productFound) {
+          throw new ResponseError({
+            statusCode: 404,
+            messages: ['Product not found'],
+          });
+        }
+
+        const productPrice = calculateProductPrice(productFound);
+        amount += productPrice * productOrder.quantity;
+      },
+    );
+
+    await Promise.all(calculateTotalPromise);
+
+    return parseFloat(amount.toFixed(2));
   }
 }
