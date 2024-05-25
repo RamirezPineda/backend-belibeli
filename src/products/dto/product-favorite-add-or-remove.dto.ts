8f8f25d130adfa9679ad1974a78b9476198dea70
RamirezPineda <ramirezpineda@gmail.com>
2024-05-24 20:59:41 -0400

    refactor(product-favorite): refactor dto

--- src/products/dto/product-favorite-add-or-remove.dto.ts
+++ src/products/dto/product-favorite-add-or-remove.dto.ts
@@ -2,10 +2,6 @@ import { z } from 'zod';
 
 export const productFavoriteAddOrRemoveDtoSchema = z
   .object({
-    id: z
-      .string({ message: 'Id is required' })
-      .uuid('Id must be of type uuid')
-      .optional(),
     productId: z
       .string({ message: 'ProductId is required' })
       .uuid('ProductId must be of type uuid'),
