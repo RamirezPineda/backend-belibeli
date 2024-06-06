8daf2005839de36457c40dbc09cf538920dac62c
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-06 19:37:27 -0400

    feat(products): add alternative a product-images

--- src/products/dto/product-update.dto.ts
+++ src/products/dto/product-update.dto.ts
@@ -2,12 +2,19 @@ import { z } from 'zod';
 import { productCreateDtoSchema } from '@/products/dto';
 
 export const productUpdateDtoSchema = productCreateDtoSchema.partial().extend({
-  productImages: z.array(
-    z
-      .string({ message: 'Product image id must be a string' })
-      .uuid('Product image id must be a valid UUID'),
-    { message: 'Product images is required' },
-  ),
+  productImages: z
+    .array(
+      z
+        .string({ message: 'Product image id must be a string' })
+        .uuid('Product image id must be a valid UUID'),
+      { message: 'Product images is required' },
+    )
+    .or(
+      z
+        .string({ message: 'Product image id must be a string' })
+        .uuid('Product image id must be a valid UUID')
+        .transform((val) => [val]),
+    ),
 });
 
 export type ProductUpdateDto = z.infer<typeof productUpdateDtoSchema>;
