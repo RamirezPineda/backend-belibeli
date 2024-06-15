cf4d9f3a885124f56025cf245a59c0a01624f798
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-15 16:26:44 -0400

    feat(analytics): add analytic controller

---
+++ src/analytics/controllers/analitytic.controller.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from 'express';
+
+import { handlerErrors } from '@/common/utils';
+import type { ResponseApi } from '@common/interfaces';
+
+import { AnalyticService } from '@/analytics/services/analitytic.service';
+
+export class AnalyticController {
+  constructor(private readonly analyticService: AnalyticService) {}
+
+  async totalNewUsersInTheMonth(req: Request, res: Response) {
+    try {
+      const totalNewUsersInTheMonth =
+        await this.analyticService.totalNewUsersInTheMonth();
+      const responseApi: ResponseApi = {
+        statusCode: 200,
+        data: totalNewUsersInTheMonth,
+      };
+
+      res.status(200).json(responseApi);
+    } catch (error) {
+      handlerErrors(res, error);
+    }
+  }
+
+  async totalSaleOfTheMonth(req: Request, res: Response) {
+    try {
+      const totalSaleOfTheMonth =
+        await this.analyticService.totalSaleOfTheMonth();
+      const responseApi: ResponseApi = {
+        statusCode: 200,
+        data: totalSaleOfTheMonth,
+      };
+
+      res.status(200).json(responseApi);
+    } catch (error) {
+      handlerErrors(res, error);
+    }
+  }
+
+  async bestSellingProductAnalytic(req: Request, res: Response) {
+    try {
+      const bestSellingProduct =
+        await this.analyticService.bestSellingProduct();
+      const responseApi: ResponseApi = {
+        statusCode: 200,
+        data: bestSellingProduct,
+      };
+
+      res.status(200).json(responseApi);
+    } catch (error) {
+      handlerErrors(res, error);
+    }
+  }
+
+  async salesOfTheYear(req: Request, res: Response) {
+    try {
+      const salesOfTheYear = await this.analyticService.salesOfTheYear();
+      const responseApi: ResponseApi = {
+        statusCode: 200,
+        data: salesOfTheYear,
+      };
+
+      res.status(200).json(responseApi);
+    } catch (error) {
+      handlerErrors(res, error);
+    }
+  }
+}
