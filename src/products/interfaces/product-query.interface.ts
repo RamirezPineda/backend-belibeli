bca5e4c0c11eee3477ed560f4a12c98ae67ba301
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-15 23:10:07 -0400

    feat(products): add best-sellet-by-product interface

--- src/products/interfaces/product-query.interface.ts
+++ src/products/interfaces/product-query.interface.ts
@@ -3,3 +3,8 @@ import type { QueryOptions } from '@/common/interfaces';
 export interface ProductQueryOptions extends QueryOptions {
   categoryId?: string;
 }
+
+export interface BestSellersByProduct {
+  productId: string;
+  quantity: string;
+}
