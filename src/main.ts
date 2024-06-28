5a9a6b1e729228880fabe9a51e76faf26a0efc31
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-28 16:02:01 -0400

    chore: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,12 +19,9 @@ let socket:
   | ServerSocket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
   | undefined;
 
-let app: Server | undefined;
-
 (async () => {
   const server = await main();
   socket = server.socketIo;
-  app = server;
 })();
 
-export { socket, app };
+export { socket };
